4f00f5555a3f60833e5899173bc696b647b80eb3
Srik <srikarrajagopalan2002@gmail.com>
2025-10-11 15:12:45 -0400

    First commit to my React repo

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,37 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state={
+        likes:0,
+        dislikes:0,
+        totalRatings:0,
+        handleLike:()=>{
+            this.setState((prevstate)=>(
+                {likes: prevstate.likes+1,
+                totalRatings: prevstate.totalRatings+1}
+            ));
+        },
+
+        handleDislike:()=>{
+            this.setState((prevstate)=>(
+                {dislikes: prevstate.dislikes+1,
+                totalRatings: prevstate.totalRatings+1}
+            ));
+        }
+    };
   }
+
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className="content-rating">
+        <p>Users can like or dislike</p>
+        <div className="rating-buttons">
+            <button className="like" onClick={this.state.handleLike}>Likes ({this.state.likes})</button><br/><br/>
+            <button className="dislike" onClick={this.state.handleDislike}>Dislikes ({this.state.dislikes})</button>
+            <p>The Total Number of Ratings is: {this.state.totalRatings}</p>
+        </div>
+     </div>
      </>
     );
   }
