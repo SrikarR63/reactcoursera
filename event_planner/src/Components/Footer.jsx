710aa155db6a94c9bc6201b806b1c4c46b4965be
Srik <srikarrajagopalan2002@gmail.com>
2025-10-16 05:56:15 -0400

    Event Planner React Webpage

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footercomp(){
+    return(
+        <>
+        <footer>
+        <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </footer>
+        </>
+    );
+}
+
+export default Footercomp;
